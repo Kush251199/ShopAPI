202a415b120cc2f50970e44742d08370e1309ff5
Kush251199 <66004046+Kush251199@users.noreply.github.com>
2021-06-02 02:49:05 +0530

    Added wishlist functionality

--- server.js
+++ server.js
@@ -34,6 +34,47 @@ app.get('/product',function(req,res) {
 	});
 });
 
+app.get('/wishlist',function(req,res){
+	WishList.find({}).populate({path:'products', model: 'Product'}).exec(function(err,wishlist){
+		if(err){
+			res.status(500).send({error:"Could not fetch wishlist."});
+		} else {
+			res.status(200).send(wishlist);
+		}
+	});
+});
+
+app.post('/wishlist',function(req,res){
+	var wishList = new WishList();
+	wishList.title = req.body.title;
+
+	wishList.save(function(err,newWishList){
+		if(err){
+			res.status(500).send({error: "Could not create wishlist."});
+		} else{
+			res.status(200).send(newWishList);
+		}
+	});
+});
+
+app.put('wishlist/product/add',function(req,res){
+	Product.findOne({_id: request.body.productId},function(err,product){
+		if(err){
+			res.status(500).send({error: "Product not found."});
+		} else{
+			WishList.update({_id:request.body.wishListId},{$addToSet:{products:product._id}},function(err,wishList){
+				if(err){
+					res.status(500).send({error: "Could not add product."});
+				}
+				else {
+					res.status(200).send(wishList);
+				}
+			});
+		}
+	});
+});
+
+
 app.listen(3000, function(){
 	console.log("Shop API running on port 3000.");
 }); 
